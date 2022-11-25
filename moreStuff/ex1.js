6e8069c027ea83bf9cd1e9598fb69f085c6bd2ef
Rich H <richardharding305@gmail.com>
2022-11-24 21:37:12 -0600

    up to more stuff ex4

---
+++ moreStuff/ex1.js
@@ -0,0 +1,6 @@
+let arr1 = [1, 2, 3];
+
+let arr2 = arr1;
+
+arr1[1] = 4;
+console.log(arr2);
---
+++ moreStuff/ex3.js
@@ -0,0 +1,4 @@
+// Write some code to give a square root of 37
+
+let sqr = Math.sqrt(37);
+console.log(sqr);
