db741d4fd0b926e42511ca5dc487f4eef076839d
Rich H <richardharding305@gmail.com>
2022-12-04 20:52:44 -0600

    notes on individual problems

--- js100/objects/ex10.js
+++ js100/objects/ex10.js
@@ -1,5 +1,6 @@
 /*
-Write a function clone that takes an object as argument and returns a shallow copy of that argument. A shallow copy returns a new object that is a copy of the original object. However, only the object itself and any properties with primitive values are duplicated. Properties that are themselves objects are copied "by reference" -- that is, only a pointer to the object is copied.
+Write a function clone that takes an object as argument and returns a shallow copy of that argument. A shallow copy returns a new object that is a copy of the original object. However, only the object itself and any properties with primitive values are duplicated. Properties that are themselves objects are copied "by reference" -- that is, only a pointer to the object is copied.  
+Review this till mastered!!!
 
 For instance, consider the following object:
 */
@@ -12,7 +13,7 @@ For instance, consider the following object:
 
 // let objcopy = {...obj};
 
-// console.log(objcopy);
+// console.log(objcopy);  
 
 
 
