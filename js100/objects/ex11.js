2bfeb593cca4efdb7d46248c6158e52709bfbd06
Rich H <richardharding305@gmail.com>
2022-12-06 21:50:19 -0600

    bs add

---
+++ js100/objects/ex11.js
@@ -0,0 +1,22 @@
+let person = {
+  firstName: 'Charlie',
+  lastName: 'Juniper',
+  'fighting style': 'brawler',
+  age: 49,
+  occupation: 'welder',
+  titles: ['AAU silver', 'UAA bronze'],
+  instuctors: {
+    boxing: 'Cus Dmata',
+    wresting: 'Dan Severn',
+    karate: 'Mr. Myagi'
+  },
+  felon: true,
+};
+
+
+function persClone(obj) {
+   return Array.
+}
+
+
+console.log(persClone(person));
